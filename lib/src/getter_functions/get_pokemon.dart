e6d8a02ab4d23928634e0d28d0edc9d9098250de
Matthew Pagan <mastapegs01@gmail.com>
2021-10-15 14:29:40 -0400

    feat: concisely import needed class (#7)

--- lib/src/getter_functions/get_pokemon.dart
+++ lib/src/getter_functions/get_pokemon.dart
@@ -1,10 +1,10 @@
 import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
-import '../../pokeapi.dart' as pokeapi;
+import '../../src/classes/pokemon.dart';
 
-Future<pokeapi.Pokemon> getPokemon(int id) async {
+Future<Pokemon> getPokemon(int id) async {
   final url = Uri.https('pokeapi.co', '/api/v2/pokemon/$id');
   final response = await http.get(url);
-  final pokemon = pokeapi.Pokemon(convert.jsonDecode(response.body));
+  final pokemon = Pokemon(convert.jsonDecode(response.body));
   return pokemon;
 }
