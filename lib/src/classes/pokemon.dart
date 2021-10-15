dddceb3f29a9deb63517679f8067e0383f4c3634
Matthew Pagan <mastapegs01@gmail.com>
2021-10-15 09:40:27 -0400

    feat: add more members to pokemon class

--- lib/src/classes/pokemon.dart
+++ lib/src/classes/pokemon.dart
@@ -2,10 +2,18 @@
 
 class Pokemon {
   final int baseExperience;
+  final int height;
   final int id;
+  final int isDefault;
   final String name;
+  final int order;
+  final int weight;
   Pokemon(dynamic json)
       : baseExperience = json['base_experience'],
+        height = json['height'],
         id = json['id'],
-        name = json['name'];
+        isDefault = json['is_default'],
+        name = json['name'],
+        order = json['order'],
+        weight = json['weight'];
 }
