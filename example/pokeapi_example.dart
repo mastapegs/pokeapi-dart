a5d4971d657158dc316ee7ca5c345730057d9925
Matthew Pagan <mastapegs01@gmail.com>
2021-10-14 21:06:37 -0400

    feat: add more members to pokemon class

--- example/pokeapi_example.dart
+++ example/pokeapi_example.dart
@@ -2,5 +2,5 @@ import 'package:pokeapi/pokeapi.dart' as pokeapi;
 
 void main() async {
   var pokemon = await pokeapi.getPokemon(1);
-  print('Pokemon: ${pokemon.name}');
+  print('Pokemon: $pokemon');
 }
