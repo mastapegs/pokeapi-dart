8f22d6d525e9c530ac33125a10fb3090166ea8c6
Matthew Pagan <mastapegs01@gmail.com>
2021-10-15 14:55:11 -0400

    feat: add more members to Pokemon class (#8)

--- lib/src/classes/pokemon.dart
+++ lib/src/classes/pokemon.dart
@@ -1,5 +1,13 @@
 /// Data for endpoint pokemon/{id or name}/
 
+class VersionGameIndex {
+  final int gameIndex;
+  final NamedAPIResource version;
+  VersionGameIndex(dynamic json)
+      : gameIndex = json['game_index'],
+        version = NamedAPIResource(json['version']);
+}
+
 class NamedAPIResource {
   final String name;
   final String url;
@@ -21,6 +29,8 @@ class PokemonAbility {
 class Pokemon {
   final List<PokemonAbility> abilities;
   final int baseExperience;
+  final List<NamedAPIResource> forms;
+  final List<VersionGameIndex> gameIndices;
   final int height;
   final int id;
   final bool isDefault;
@@ -32,6 +42,10 @@ class Pokemon {
       : abilities = List<PokemonAbility>.from(
             json['abilities'].map((ability) => PokemonAbility(ability))),
         baseExperience = json['base_experience'],
+        forms = List<NamedAPIResource>.from(
+            json['forms'].map((form) => NamedAPIResource(form))),
+        gameIndices = List<VersionGameIndex>.from(json['game_indices']
+            .map((gameIndex) => VersionGameIndex(gameIndex))),
         height = json['height'],
         id = json['id'],
         isDefault = json['is_default'],
