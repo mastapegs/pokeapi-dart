25c860cf278f253963bef73f3e290622c5e06f5a
Matthew Pagan <mastapegs01@gmail.com>
2021-10-14 21:09:00 -0400

    feat: add more members to pokemon class (#3)

--- lib/src/classes/pokemon.dart
+++ lib/src/classes/pokemon.dart
@@ -1,8 +1,12 @@
 /// Data for endpoint pokemon/{id or name}/
 
 class Pokemon {
+  int? baseExperience;
+  int? id;
   String? name;
   Pokemon(dynamic json) {
     name = json['name'];
+    id = json['id'];
+    baseExperience = json['base_experience'];
   }
 }
